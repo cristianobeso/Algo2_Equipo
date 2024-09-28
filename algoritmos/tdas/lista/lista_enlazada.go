a17e53e8fa5943c54b3d33cb48b8d3dd97b67ab0
Cristian Obeso Calderon <cobeso@fi.uba.ar>
2024-09-28 20:43:14 -0300

    Primera version de las primitivas del iterador

--- algoritmos/tdas/lista/lista_enlazada.go
+++ algoritmos/tdas/lista/lista_enlazada.go
@@ -11,6 +11,14 @@ type listaEnlazada[T any] struct {
 	largo   int
 }
 
+type iterListaEnlazada[T any] struct {
+	anterior *nodoLista[T]
+	actual   *nodoLista[T]
+	lista    *listaEnlazada[T]
+	//Tengo en mis apuntes que el iterador tenia mas o menos esta estructura
+	//Pero me quedan dudas porque para las primitivas no use 'lista'
+}
+
 func crearNodo[T any](dato T, ptr *nodoLista[T]) *nodoLista[T] {
 	return &nodoLista[T]{dato: dato, siguiente: ptr}
 }
@@ -18,6 +26,8 @@ func crearNodo[T any](dato T, ptr *nodoLista[T]) *nodoLista[T] {
 func CrearListaEnlazada[T any]() Lista[T] {
 	return &listaEnlazada[T]{primero: nil, ultimo: nil, largo: 0}
 }
+
+/**************  Primitivas de la lista  **************/
 
 func (lista *listaEnlazada[T]) EstaVacia() bool {
 	return lista.primero == nil
@@ -73,3 +83,46 @@ func (lista *listaEnlazada[T]) Largo() int {
 	}
 	return lista.largo
 }
+
+func (lista *listaEnlazada[T]) Iterar(visitar func(T) bool) {
+	//Este no tengo idea te lo dejo todo a vos
+}
+
+/**************  Fin lista  **************/
+
+/**************  Primitivas del iterador  **************/
+//No estoy seguro si esto funcione, pero es algo
+
+func (lista *listaEnlazada[T]) Iterador() IteradorLista[T] {
+	return &iterListaEnlazada[T]{anterior: nil, actual: nil, lista: lista}
+}
+
+func (iterador *iterListaEnlazada[T]) VerActual() T {
+	return iterador.actual.dato
+}
+
+func (iterador *iterListaEnlazada[T]) HaySiguiente() bool {
+	return iterador.actual.siguiente != nil
+}
+
+func (iterador *iterListaEnlazada[T]) Siguiente() {
+	ptr := iterador.actual.siguiente
+	iterador.anterior = iterador.actual
+	iterador.actual = ptr
+}
+
+func (iterador *iterListaEnlazada[T]) Insertar(elemento T) {
+	ptr := iterador.actual.siguiente
+	nuevoNodo := crearNodo(elemento, ptr)
+	iterador.actual = nuevoNodo
+}
+
+func (iterador *iterListaEnlazada[T]) Borrar() T {
+	elemento := iterador.actual.dato
+	ptr := iterador.actual.siguiente
+	iterador.anterior.siguiente = ptr
+	iterador.actual = ptr
+	return elemento
+}
+
+/**************  Fin iterador  **************/
