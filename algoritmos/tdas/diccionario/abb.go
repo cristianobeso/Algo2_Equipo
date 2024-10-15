fa8e68b944fd1bf2af5b601d60613251d919e600
Cristian Obeso Calderon <cobeso@fi.uba.ar>
2024-10-15 14:26:50 -0300

    Las funciones que haremos, subire en la noche la mayoria

--- algoritmos/tdas/diccionario/abb.go
+++ algoritmos/tdas/diccionario/abb.go
@@ -10,27 +10,47 @@ type nodoAbb[K comparable, V any] struct {
 type abb[K comparable, V any] struct {
 	raiz     *nodoAbb[K, V]
 	cantidad int
-	cmp      funcCmp[K]
+	cmp      funcCmp[K] // no estoy seguro de esto
 }
 
-//No se que hice
-// type iteradorABB[K comparable, V any] struct {
-// 	abb    *abb[K, V]
-// 	indice int
-// }
+// Hay que hacer todas las funciones otra vez
 
-// func CrearABB[K comparable, V any](function_cmp func(K, K) int) DiccionarioOrdenado[K, V] {
-// 	return &abb[K, V]{raiz: nil, cantidad: 0, cmp: function_cmp}
-// }
+func CrearABB[K comparable, V any](function_cmp func(K, K) int) DiccionarioOrdenado[K, V] {
+	return &abb[K, V]{raiz: nil, cantidad: 0, funcCmp: function_cmp}
+}
 
-// func (abb *abb[K, V]) IterarRango(desde *K, hasta *K, visitar func(clave K, dato V) bool) {
+func (abb *abb[K, V]) Guardar(clave K, dato V) {
+	//
+}
 
-// }
+func (abb *abb[K, V]) Pertenece(clave K) bool {
+	return true
+}
 
-// func (abb *abb[K, V]) IteradorRango(desde *K, hasta *K) IterDiccionario[K, V] {
-// 	return abb.Iterador()
-// }
+func (abb *abb[K, V]) Obtener(clave K) V {
+	return abb.raiz.dato // por ahora para no marcar error en el ide
+}
 
-// func (iter *iteradorABB[K, V]) avanzar(){
-// 	if iter.abb.raiz.izquierdo
-// }
+func (abb *abb[K, V]) Borrar(clave K) V {
+	return abb.raiz.dato // por ahora
+}
+
+func (abb *abb[K, V]) Cantidad() int {
+	return abb.cantidad
+}
+
+func (abb *abb[K, V]) Iterar(func(clave K, dato V) bool) {
+	//
+}
+
+func (abb *abb[K, V]) Iterador() IterDiccionario[K, V] {
+	//
+}
+
+func (abb *abb[K, V]) IterarRango(desde *K, hasta *K, visitar func(clave K, dato V) bool) {
+	//
+}
+
+func (abb *abb[K, V]) IteradorRango(desde *K, hasta *K) IterDiccionario[K, V] {
+
+}
