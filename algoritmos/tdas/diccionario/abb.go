6e566fffda2bbb81108ff3a6b69291e2639af5f2
Cristian Obeso Calderon <cobeso@fi.uba.ar>
2024-10-16 15:25:56 -0300

    Una forma de encontrar el lugar donde insertar un nuevo elemento, pero funcionaria para borrar

--- algoritmos/tdas/diccionario/abb.go
+++ algoritmos/tdas/diccionario/abb.go
@@ -19,10 +19,40 @@ func CrearABB[K comparable, V any](function_cmp func(K, K) int) DiccionarioOrden
 	return &abb[K, V]{raiz: nil, cantidad: 0, funcCmp: function_cmp}
 }
 
-func (abb *abb[K, V]) Guardar(clave K, dato V) {
-	if !abb.Pertenece(clave) {
-		//inserto
+func crearNodo[K comparable, V any](clave K, dato V) *nodoAbb[K, V] {
+	return &nodoAbb[K, V]{izquierdo: nil, derecho: nil, clave: clave, dato: dato}
+}
+
+// Retorna el nodo padre y en que lugar insertar un hijo
+func (nodo *nodoAbb[K, V]) ubicar(clave K, ptrPadre *nodoAbb[K, V], num int, funcCmp func(K, K) int) (*nodoAbb[K, V], int) {
+	if nodo == nil {
+		return ptrPadre, num
 	}
+	if nodo.clave == clave {
+		return nodo, 0
+	}
+	if funcCmp(nodo.clave, clave) > 0 { // Se supone que la comparacion devuelve un valor positivo si el primero es mas grande
+		return nodo.izquierdo.ubicar(clave, nodo, -1, funcCmp)
+	} else {
+		return nodo.derecho.ubicar(clave, nodo, 1, funcCmp)
+	}
+}
+
+func (abb *abb[K, V]) Guardar(clave K, dato V) {
+	ptr, dir := abb.raiz.ubicar(clave, abb.raiz, 0, abb.funcCmp)
+	nuevoNodo := crearNodo(clave, dato)
+	if ptr == nil {
+		abb.raiz = nuevoNodo
+	}
+	if dir == -1 {
+		(*ptr).izquierdo = nuevoNodo
+	} else if dir == 1 {
+		(*ptr).derecho = nuevoNodo
+	} else {
+		(*ptr).dato = dato
+	}
+	abb.cantidad++
+	// despues habrai que crear una funcion para rotar
 }
 
 func (abb *abb[K, V]) Pertenece(clave K) bool {
@@ -35,6 +65,21 @@ func (abb *abb[K, V]) Pertenece(clave K) bool {
 		return true
 	})
 	return cont+1 != abb.cantidad // Si el indice es distinto significa que se corto la iteracion por ende encontro el elemento
+}
+
+func (abb *abb[K, V]) PerteneceV2(clave K) bool {
+	ptr, dir := abb.raiz.ubicar(clave, abb.raiz, 0, abb.funcCmp)
+	return ptr != nil && dir == 0 // Si al ubicarlo el ptr no es nulo y la direccion es 0
+	// significa que ese puntero es el que tiene la clave buscada
+}
+
+func (abb *abb[K, V]) ObtenerV2(clave K) V {
+	ptr, dir := abb.raiz.ubicar(clave, abb.raiz, 0, abb.funcCmp)
+
+	if ptr != nil && dir == 0 {
+		return (*ptr).dato
+	}
+	panic("NO encontrado")
 }
 
 func (abb *abb[K, V]) Obtener(clave K) V {
