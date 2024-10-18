77793cf5416e422bc9a736bcbb1255adc336a6d1
Cristian Obeso Calderon <cobeso@fi.uba.ar>
2024-10-17 23:16:15 -0300

    Prueba para verificar el orden

--- algoritmos/tdas/diccionario/diccionario_ordenado_test.go
+++ algoritmos/tdas/diccionario/diccionario_ordenado_test.go
@@ -62,3 +62,73 @@ func TestDiccionario(t *testing.T) {
 		return true
 	})
 }
+
+func TestDiccionarioOrden(t *testing.T) {
+	dic := TDADiccionario.CrearABB[int, int](func(elemento1, elemento2 int) int {
+		if elemento1 < elemento2 {
+			return -1
+		} else if elemento1 > elemento2 {
+			return 1
+		} else {
+			return 0
+		}
+	})
+
+	dic.Guardar(20, 123)
+
+	dic.Guardar(10, 456)
+	dic.Guardar(15, 789)
+	dic.Guardar(5, 159)
+	dic.Guardar(3, 753)
+	dic.Guardar(6, 486)
+
+	dic.Guardar(30, 426)
+	dic.Guardar(40, 423)
+	dic.Guardar(35, 126)
+	dic.Guardar(22, 781)
+
+	iter := dic.Iterador()
+	k, v := iter.VerActual()
+	require.Equal(t, k, 3)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 5)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 6)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 10)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 15)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 20)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 22)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 30)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 35)
+	iter.Siguiente()
+
+	k, v = iter.VerActual()
+	require.Equal(t, k, 40)
+
+	require.Equal(t, 423, v)
+	// La verdad estoy sorprendido de que hay pasado la prueba
+	// Sigo sintiendo que hay algo mal
+	// Hare mas pruebas
+}
