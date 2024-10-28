71d1a7741373f17609eddd972bd8575350764420
Nico Ferreiro <nferreiro@fi.uba.ar>
2024-10-28 20:27:37 -0300

    Cambio struct

--- algoritmos/tdas/cola_prioridad/heap.go
+++ algoritmos/tdas/cola_prioridad/heap.go
@@ -1,61 +1,64 @@
 package cola_prioridad
 
-type Heap[T any] struct {
-	elementos []T
-	cmp       func(T, T) int
+type Heap[T any] struct { // cambie el struct porq asi lo decia en el video
+	datos []T
+	cant  int
+	cmp   func(T, T) int
 }
 
 func CrearHeap[T any](funcion_cmp func(T, T) int) ColaPrioridad[T] {
-	return &Heap[T]{elementos: []T{}, cmp: funcion_cmp}
+	return &Heap[T]{datos: []T{}, cant: 0, cmp: funcion_cmp}
 }
 
 func CrearHeapArr[T any](arreglo []T, funcion_cmp func(T, T) int) ColaPrioridad[T] {
-	h := &Heap[T]{elementos: arreglo, cmp: funcion_cmp}
-	for i := len(h.elementos)/2 - 1; i >= 0; i-- {
+	h := &Heap[T]{datos: arreglo, cant: len(arreglo), cmp: funcion_cmp}
+	for i := len(h.datos)/2 - 1; i >= 0; i-- {
 		h.heapifyDown(i)
 	}
 	return h
 }
 
 func (h *Heap[T]) EstaVacia() bool {
-	return len(h.elementos) == 0
+	return h.cant == 0
 }
 
 func (h *Heap[T]) Encolar(valor T) {
-	h.elementos = append(h.elementos, valor)
-	h.heapifyUp(len(h.elementos) - 1)
+	h.datos = append(h.datos, valor)
+	h.cant++
+	h.heapifyUp(h.cant - 1)
 }
 
 func (h *Heap[T]) VerMax() T {
 	if h.EstaVacia() {
 		panic("La cola está vacía")
 	}
-	return h.elementos[0]
+	return h.datos[0]
 }
 
 func (h *Heap[T]) Desencolar() T {
 	if h.EstaVacia() {
 		panic("La cola está vacía")
 	}
-	max := h.elementos[0]
-	h.intercambiar(0, len(h.elementos)-1)
-	h.elementos = h.elementos[:len(h.elementos)-1]
+	max := h.datos[0]
+	h.intercambiar(0, h.cant-1)
+	h.datos = h.datos[:h.cant-1]
+	h.cant--
 	h.heapifyDown(0)
 	return max
 }
 
 func (h *Heap[T]) Cantidad() int {
-	return len(h.elementos)
+	return h.cant
 }
 
 func (h *Heap[T]) intercambiar(i, j int) {
-	h.elementos[i], h.elementos[j] = h.elementos[j], h.elementos[i]
+	h.datos[i], h.datos[j] = h.datos[j], h.datos[i]
 }
 
 func (h *Heap[T]) heapifyUp(pos int) {
 	for pos > 0 {
 		padre := (pos - 1) / 2
-		if h.cmp(h.elementos[pos], h.elementos[padre]) <= 0 {
+		if h.cmp(h.datos[pos], h.datos[padre]) <= 0 {
 			break
 		}
 		h.intercambiar(pos, padre)
@@ -64,16 +67,16 @@ func (h *Heap[T]) heapifyUp(pos int) {
 }
 
 func (h *Heap[T]) heapifyDown(pos int) {
-	ultimo := len(h.elementos) - 1
+	ultimo := h.cant - 1
 	for {
 		hijoIzq := 2*pos + 1
 		hijoDer := 2*pos + 2
 		mayor := pos
 
-		if hijoIzq <= ultimo && h.cmp(h.elementos[hijoIzq], h.elementos[mayor]) > 0 {
+		if hijoIzq <= ultimo && h.cmp(h.datos[hijoIzq], h.datos[mayor]) > 0 {
 			mayor = hijoIzq
 		}
-		if hijoDer <= ultimo && h.cmp(h.elementos[hijoDer], h.elementos[mayor]) > 0 {
+		if hijoDer <= ultimo && h.cmp(h.datos[hijoDer], h.datos[mayor]) > 0 {
 			mayor = hijoDer
 		}
 		if mayor == pos {
@@ -85,12 +88,11 @@ func (h *Heap[T]) heapifyDown(pos int) {
 }
 
 func HeapSort[T any](elementos []T, funcion_cmp func(T, T) int) {
-
 	heap := CrearHeapArr(elementos, funcion_cmp).(*Heap[T])
 
 	for i := len(elementos) - 1; i > 0; i-- {
 		heap.intercambiar(0, i)
-		heap.elementos = heap.elementos[:i]
+		heap.cant--
 		heap.heapifyDown(0)
 	}
 }
