ddac91027431f4642f85da4f79aa835fa914b55c
Nico Ferreiro <nferreiro@fi.uba.ar>
2024-11-15 19:36:40 -0300

    pres y postcondiciones

--- algoritmos/TP2/go.mod
+++
@@ -1,9 +0,0 @@
-module TP2
-
-go 1.22
-
-toolchain go1.23.2
-
-replace tdas => ../tdas
-
-require tdas v0.0.0-00010101000000-000000000000
--- algoritmos/TP2/servidor/tdaServidor.go
+++
@@ -1,7 +0,0 @@
-package servidor
-
-type DatosServidor interface {
-	AgregarArchivo(nombre_archivo string) error
-	VerVisitantes(desde, hasta string)
-	VerMasVisitados(n int)
-}
