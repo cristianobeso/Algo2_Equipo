d39c92ab2df691f55ef81378293dc7cee691d225
Nico Ferreiro <nferreiro@fi.uba.ar>
2024-11-15 19:23:50 -0300

    Agrego pres y postcondiciones

--- algoritmos/TP2/servidor/servidor.go
+++ algoritmos/TP2/servidor/servidor.go
@@ -32,13 +32,30 @@ type entrada struct {
 type Servidor struct {
 	visitantes TDADiccionario.DiccionarioOrdenado[string, entrada]
 	visitados  TDADiccionario.Diccionario[string, int]
-	denegados  TDADiccionario.Diccionario[string, string] // quizas seria mejor un heap
+	denegados  TDADiccionario.Diccionario[string, string]
 }
 
+/*
+Precondiciones: No requiere parámetros ni dependencias previas.
+
+Postcondiciones: Devuelve una instancia de Servidor inicializada con, un diccionario ordenado para visitantes
+y un diccionario hash para visitados.
+*/
 func CrearEstructuraDatos() DatosServidor {
 	return &Servidor{visitantes: TDADiccionario.CrearABB[string, entrada](CompareIPs), visitados: TDADiccionario.CrearHash[string, int]()}
 }
 
+/*
+Precondiciones: El archivo especificado por el parámetro archivo debe existir y ser accesible.
+El formato del archivo debe consistir en líneas tabuladas con cuatro columnas, Dirección IP (string),
+Fecha y hora en el formato "2006-01-02T15:04:05-07:00", Método de la solicitud (string) y Recurso solicitado (string).
+
+Postcondiciones: Los visitantes y sus datos asociados se almacenan en visitantes.
+Los recursos solicitados y sus conteos se actualizan en visitados.
+Las IPs sospechosas de DoS se almacenan en denegados.
+Si no hay errores, imprime DoS para las IPs sospechosas en orden y retorna nil.
+Si hay errores en el archivo o el formato, retorna un error descriptivo.
+*/
 func (serv *Servidor) AgregarArchivo(archivo string) error {
 	serv.denegados = TDADiccionario.CrearHash[string, string]()
 	file, err := os.Open(archivo)
@@ -67,8 +84,8 @@ func (serv *Servidor) AgregarArchivo(archivo string) error {
 
 		if serv.visitantes.Pertenece(ip) {
 			datos := serv.visitantes.Obtener(ip)
-			//El tiempo actual siempre es mayor al tiempo de la anterior peticion
-			if tiempo.Sub(datos.tiempo).Seconds() < 2 && tiempo.Sub(datos.tiempo).Seconds() >= 0 { // si la diferencia es menor a 2 segundos
+
+			if tiempo.Sub(datos.tiempo).Seconds() < 2 && tiempo.Sub(datos.tiempo).Seconds() >= 0 {
 				cant = datos.cantidadPetisiones + 1
 				tiempo = datos.tiempo
 				if cant >= 5 && !serv.denegados.Pertenece(ip) {
@@ -89,7 +106,6 @@ func (serv *Servidor) AgregarArchivo(archivo string) error {
 
 	}
 
-	// ****************
 	iter := serv.denegados.Iterador()
 	arr := make([]string, 0)
 	for iter.HaySiguiente() {
@@ -101,18 +117,29 @@ func (serv *Servidor) AgregarArchivo(archivo string) error {
 	for i := 0; i < len(arr); i++ {
 		fmt.Printf("DoS: %s\n", arr[i])
 	}
-	//*****************
 
 	fmt.Println("OK")
 	return nil
 }
 
+/*
+Precondiciones: desde y hasta deben ser direcciones IP válidas.
+
+Postcondiciones: Imprime en la consola todas las IPs entre el rango [desde, hasta] presentes en el diccionario visitantes.
+*/
 func (serv *Servidor) VerVisitantes(desde, hasta string) {
 	fmt.Println("Visitantes:")
 	serv.visitantes.IterarRango(&desde, &hasta, func(clave string, dato entrada) bool { fmt.Printf("\t%s\n", clave); return true })
 	fmt.Println("OK")
 }
 
+/*
+Precondiciones: n debe ser un número entero mayor o igual a 1.
+visitados debe contener al menos n recursos distintos si se desean mostrar n elementos.
+
+Postcondiciones: Imprime los n recursos más visitados en la consola junto con sus conteos.
+Si hay menos de n recursos, muestra todos los disponibles.
+*/
 func (serv *Servidor) VerMasVisitados(n int) {
 	type Dato struct {
 		cantidad int
@@ -137,7 +164,12 @@ func (serv *Servidor) VerMasVisitados(n int) {
 
 }
 
-// Compara dos IPs numéricamente.
+/*
+Precondiciones: ip1 y ip2 deben ser cadenas que representen direcciones IP.
+
+Postcondiciones: Devuelve,un valor negativo si ip1 es menor que ip2, cero si son iguales,
+un valor positivo si ip1 es mayor que ip2.
+*/
 func CompareIPs(ip1, ip2 string) int {
 	parts1 := strings.Split(ip1, ".")
 	parts2 := strings.Split(ip2, ".")
@@ -151,6 +183,12 @@ func CompareIPs(ip1, ip2 string) int {
 	return 0
 }
 
+/*
+Precondiciones: `s` debe ser una cadena de texto que represente un número entero válido en base 10.
+
+Postcondiciones: Devuelve el número entero representado por la cadena `s`. Si la cadena no es un número
+entero válido, devuelve 0.
+*/
 func Atoi(s string) int {
 	i, _ := strconv.Atoi(s)
 	return i
